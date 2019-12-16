eac1258d10c079ee62dc8280ca3b90f52e028336
Ubaldo Puocci <u.puocci@gmail.com>
2019-12-16 21:17:15 +0100

    added hadoop home dir

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,7 @@ public class main {
     private static final String NAME_NODE = "hdfs://localhost:9000";
 
     public static void main(String[] args) throws URISyntaxException, IOException {
+        System.setProperty("hadoop.home.dir", "/home/eyedema/hadoop");
         HDFSFileReader hdfsfr = new HDFSFileReader(NAME_NODE, args[0]);
         hdfsfr.readBooks();
     }
