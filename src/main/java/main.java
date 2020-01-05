626b90d2d72479ade249a0204cd41057e53c7d4b
Ubaldo Puocci <u.puocci@gmail.com>
2020-01-05 13:10:44 +0100

    added mapper and reducer v1.0

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,11 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public class main {
 
@@ -8,7 +14,15 @@ public class main {
     public static void main(String[] args) throws URISyntaxException, IOException {
         System.setProperty("hadoop.home.dir", "/home/eyedema/hadoop");
         HDFSFileReader hdfsfr = new HDFSFileReader(NAME_NODE, args[0]);
-        hdfsfr.readBooks();
+        ArrayList<String> books = hdfsfr.readBooks();
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "N-gram calculation");
+        job.setJarByClass(main.class);
+        job.setMapperClass(myMapper.class);
+        job.setCombinerClass(myReducer.class);
+        job.setReducerClass(myReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
     }
 
 }
---
+++ src/main/java/myMapper.java
@@ -0,0 +1,36 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class myMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text ngramWord = new Text();
+    private Text currentWord = new Text();
+
+    public static List<String> ngrams(int n, String str) {
+        List<String> ngrams = new ArrayList<String>();
+        for (int i = 0; i < str.length() - n + 1; i++) {
+            ngrams.add(str.substring(i, i + n));
+        }
+        return ngrams;
+    }
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            currentWord.set(itr.nextToken());
+            List<String> currentWordNGrams = ngrams(3, currentWord.toString());
+            for (String ngram : currentWordNGrams) {
+                ngramWord.set(ngram);
+                context.write(ngramWord, one);
+            }
+        }
+    }
+
+}
